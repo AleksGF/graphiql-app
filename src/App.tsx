ad7db512dabda020829db0e3a8038ca72e614777
AleksGF <aleksgf@mail.zp.ua>
2023-12-06 17:53:24 +0200

    refactor: move Router inside of Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import Layout from '@/components/Layout/Layout';
+import React from 'react';
 import LanguageContextProvider from '@/components/context/LanguageContext/LanguageContextProvider';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { routes } from '@/routes/routes';
+
+const router = createBrowserRouter(routes);
 
 export default function App() {
   return (
     <LanguageContextProvider>
-      <Layout />
+      <RouterProvider router={router} />
     </LanguageContextProvider>
   );
 }
