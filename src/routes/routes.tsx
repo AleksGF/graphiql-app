3ad78066caf0c57465ee3db8d97370c993f783f0
DanilWork <bogdanovdanila1404@gmail.com>
2023-12-06 15:38:59 +0500

    feat: add export to RoutePaths

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -5,7 +5,7 @@ import MainPage from '@/pages/MainPage/MainPage';
 import SignInPage from '@/pages/SignInPage/SignInPage';
 import SignUpPage from '@/pages/SignUpPage/SignUpPage';
 
-enum RoutePaths {
+export enum RoutePaths {
   IndexPage = '/',
   MainPage = '/main',
   SignInPage = '/signin',
