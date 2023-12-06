24553f5bead74df77f0b1f86e4a645fcf8de911a
DanilWork <bogdanovdanila1404@gmail.com>
2023-12-06 16:52:17 +0500

    feat: add userForm to SignPage

--- src/pages/SignInPage/SignInPage.tsx
+++ src/pages/SignInPage/SignInPage.tsx
@@ -1,4 +1,40 @@
-// TODO implement SignIn Page
+import { useNavigate } from 'react-router';
+import { setUser } from '@/store/reducers/userSlice';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import UserForm from '@/components/forms/userForm/UserForm';
+import { useAppDispatch, useAppSelector } from '@/hooks/hooks';
+import { RoutePaths } from '@/routes/routes';
+import { useEffect } from 'react';
+
 export default function SignInPage() {
-  return <main>SignIn Page</main>;
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const { user } = useAppSelector((state) => state.user);
+
+  useEffect(() => {
+    if (user) {
+      navigate(RoutePaths.MainPage);
+    }
+  }, [navigate, user]);
+
+  const handleLogin = async (email: string, pass: string) => {
+    const auth = getAuth();
+
+    try {
+      const { user } = await signInWithEmailAndPassword(auth, email, pass);
+
+      if (user.email) {
+        dispatch(setUser({ email: user.email, id: user.uid }));
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <main>
+      <h2>Login</h2>
+      <UserForm onSubmit={handleLogin} />
+    </main>
+  );
 }
