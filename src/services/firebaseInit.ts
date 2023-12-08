654d38dd0975329b7f53b50771f36bfa3e5509b8
DanilWork <bogdanovdanila1404@gmail.com>
2023-12-08 12:49:23 +0500

    refactor: change config to env

--- src/services/firebaseInit.ts
+++ src/services/firebaseInit.ts
@@ -1,4 +1,12 @@
-import { firebaseConfig } from '@/firebaseConfig';
 import { initializeApp } from 'firebase/app';
 
-initializeApp(firebaseConfig);
+const config = {
+  apiKey: import.meta.env.VITE_FB_API_KEY,
+  authDomain: import.meta.env.VITE_FB_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FB_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FB_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FB_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FB_APP_ID,
+};
+
+initializeApp(config);
