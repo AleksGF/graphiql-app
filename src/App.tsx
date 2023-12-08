49edd77a164f0d61dc6bfb5903a8618a9c01289a
DanilWork <bogdanovdanila1404@gmail.com>
2023-12-08 12:53:56 +0500

    feat: add dispatching userState changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
+import { useEffect } from 'react';
 import Layout from '@/components/Layout/Layout';
+import { setUser } from './store/reducers/userSlice';
+import { useAuthState } from './hooks/auth';
+import { useAppDispatch } from './hooks/hooks';
 import '@/services/firebaseInit';
 
 export default function App() {
+  const dispatch = useAppDispatch();
+  const [user] = useAuthState();
+
+  useEffect(() => {
+    dispatch(setUser(user));
+  }, [dispatch, user]);
+
   return <Layout />;
 }
