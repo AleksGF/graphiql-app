633eceb2a678bbdf30a3b084370e4fbfd0486aba
DanilWork <bogdanovdanila1404@gmail.com>
2023-12-13 14:45:50 +0500

    feat: add mui to user form

--- src/components/forms/userForm/UserForm.tsx
+++ src/components/forms/userForm/UserForm.tsx
@@ -1,34 +1,92 @@
-import { useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Box, Avatar, Typography, TextField, Button } from '@mui/material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { schema } from '@/types/validationSchema';
 
 type FormProps = {
+  title: string;
   onSubmit: (email: string, pass: string) => void;
 };
 
-// TODO Implement Client-side validation
-export default function UserForm({ onSubmit }: FormProps) {
-  const [email, setEmail] = useState<string>('');
-  const [pass, setPass] = useState<string>('');
+type FormInput = {
+  email: string;
+  password: string;
+};
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    onSubmit(email, pass);
+export default function UserForm({ title, onSubmit }: FormProps) {
+  const { control, handleSubmit } = useForm<FormInput>({
+    resolver: yupResolver<FormInput>(schema),
+    defaultValues: { email: '', password: '' },
+    mode: 'all',
+  });
+
+  const onSubmitForm = ({ email, password }: FormInput) => {
+    onSubmit(email, password);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="email"
-        value={email}
-        placeholder="email"
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        value={pass}
-        placeholder="password"
-        onChange={(e) => setPass(e.target.value)}
-      />
-      <input type="submit" value="Submit" />
-    </form>
+    <Box
+      sx={{
+        marginTop: 4,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+        <LockOutlinedIcon />
+      </Avatar>
+      <Typography component="h2" variant="h5">
+        {title}
+      </Typography>
+      <Box
+        component="form"
+        onSubmit={handleSubmit(onSubmitForm)}
+        noValidate
+        sx={{ mt: 1 }}
+      >
+        <Controller
+          name="email"
+          control={control}
+          render={({ field, fieldState: { error } }) => (
+            <TextField
+              error={!!error}
+              margin="normal"
+              required
+              fullWidth
+              label="Email"
+              autoFocus
+              {...field}
+              helperText={error?.message}
+            />
+          )}
+        />
+        <Controller
+          name="password"
+          control={control}
+          render={({ field, fieldState: { error } }) => (
+            <TextField
+              error={!!error}
+              margin="normal"
+              required
+              fullWidth
+              label="Password"
+              type="password"
+              {...field}
+              helperText={error?.message}
+            />
+          )}
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          {title.toUpperCase()}
+        </Button>
+      </Box>
+    </Box>
   );
 }
