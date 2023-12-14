469f764a96f765273b1bb5158e1a156ba72c6277
DanilWork <bogdanovdanila1404@gmail.com>
2023-12-14 15:14:40 +0500

    feat: add localization to SignUp page

--- src/pages/SignUpPage/SignUpPage.tsx
+++ src/pages/SignUpPage/SignUpPage.tsx
@@ -1,22 +1,50 @@
+import { useState } from 'react';
 import UserForm from '@/components/forms/userForm/UserForm';
+import { useLanguageContext } from '@/components/context';
+import { LANGUAGES } from '@/constants/dictionaries';
 import { RoutePaths } from '@/routes/routes';
+import { ERROR_SHOW_TIME } from '@/constants/form';
 import { signUp } from '@/services/authService';
-import { Container, Link } from '@mui/material';
+import { Container, Link, Alert, Fade } from '@mui/material';
 
 export default function SignUpPage() {
+  const { language } = useLanguageContext();
+  const [isAlertShow, setIsAlertShow] = useState<boolean>(false);
+  const [timerId, setTimerId] = useState<number | null>(null);
+
+  const showAlert = () => {
+    if (timerId) {
+      clearTimeout(timerId);
+    }
+
+    const timer = setTimeout(() => {
+      setIsAlertShow(false);
+      setTimerId(null);
+    }, ERROR_SHOW_TIME);
+
+    setTimerId(+timer);
+    setIsAlertShow(true);
+  };
+
   const handleSubmit = async (email: string, pass: string) => {
     try {
       await signUp(email, pass);
-    } catch (e) {
-      console.log(e); // TODO Implement user friendly message
+    } catch {
+      showAlert();
     }
   };
 
   return (
     <Container component="main" maxWidth="xs" sx={{ flex: 1, mb: 8 }}>
-      <UserForm title={'Sign Up'} onSubmit={handleSubmit} />
+      <Fade in={isAlertShow}>
+        <Alert severity="error">{LANGUAGES[language].SIGNUP_MESSAGE}</Alert>
+      </Fade>
+      <UserForm
+        title={LANGUAGES[language].SIGNUP_TITLE}
+        onSubmit={handleSubmit}
+      />
       <Link href={RoutePaths.SignInPage} variant="body2">
-        {'Already have an account? Sign in'}
+        {LANGUAGES[language].SIGNUP_LINK}
       </Link>
     </Container>
   );
