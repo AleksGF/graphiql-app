5c15895deac52e812d5b4fe1f58b1c6d9b3fcd7f
Sergey Vergun <sewergun@gmail.com>
2023-12-15 14:44:18 +0300

    feat: add redux slice for editor content

--- src/components/QueryEditor/QueryEditor.tsx
+++ src/components/QueryEditor/QueryEditor.tsx
@@ -3,20 +3,37 @@ import { material } from '@uiw/codemirror-theme-material';
 import { graphql } from 'cm6-graphql';
 import { LANGUAGES } from '@/constants/dictionaries';
 import { useLanguageContext } from '@/context';
+import { useCallback, useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from '@/hooks/hooks';
+import { setContent } from '@/store/reducers/queryEditorSlice';
 
 interface QueryEditorProps {
   height?: string;
 }
 
 export default function QueryEditor({ height = '200px' }: QueryEditorProps) {
+  const { content } = useAppSelector((state) => state.queryEditor);
+  const dispatch = useAppDispatch();
   const { language } = useLanguageContext();
+
+  useEffect(() => {
+    dispatch(setContent(LANGUAGES[language].QUERY_EDITOR_PLACEHOLDER));
+  }, [dispatch, language]);
+
+  const onChange = useCallback(
+    (value: string) => {
+      dispatch(setContent(value));
+    },
+    [dispatch],
+  );
 
   return (
     <CodeMirror
-      value={LANGUAGES[language].QUERY_EDITOR_PLACEHOLDER}
+      value={content}
       height={height}
       theme={material}
       extensions={[graphql()]}
+      onChange={onChange}
     />
   );
 }
