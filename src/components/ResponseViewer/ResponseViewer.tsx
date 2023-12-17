f27ddd2e3ecec4658dff62ff6b6ea54eecdd687e
Sergey Vergun <sewergun@gmail.com>
2023-12-17 19:21:25 +0300

    feat: add theme switching

--- src/components/ResponseViewer/ResponseViewer.tsx
+++ src/components/ResponseViewer/ResponseViewer.tsx
@@ -1,8 +1,9 @@
 import CodeMirror from '@uiw/react-codemirror';
-import { material } from '@uiw/codemirror-theme-material';
 import { json } from '@codemirror/lang-json';
 import { LANGUAGES } from '@/constants/dictionaries';
 import { useLanguageContext } from '@/context';
+import { materialLight, materialDark } from '@uiw/codemirror-theme-material';
+import { useTheme } from '@mui/material';
 
 interface ResponseViewerProps {
   height?: string;
@@ -12,13 +13,14 @@ export default function ResponseViewer({
   height = '100%',
 }: ResponseViewerProps) {
   const { language } = useLanguageContext();
+  const theme = useTheme();
 
   return (
     <CodeMirror
       style={{ height: '100%' }}
       value={LANGUAGES[language].RESPONSE_VIEWER_PLACEHOLDER}
       height={height}
-      theme={material}
+      theme={theme.palette.mode === 'dark' ? materialDark : materialLight}
       extensions={[json()]}
       readOnly
     />
