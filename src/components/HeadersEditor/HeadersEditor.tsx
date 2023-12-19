9a51d7da46f5719e52f9786a6428eb0708e10457
AleksGF <aleksgf@mail.zp.ua>
2023-12-19 14:06:19 +0200

    refactor: remome headers checking in editor

--- src/components/HeadersEditor/HeadersEditor.tsx
+++ src/components/HeadersEditor/HeadersEditor.tsx
@@ -1,59 +1,25 @@
-import React, { useCallback, useState } from 'react';
-import { useAppDispatch, useAppSelector } from '@/hooks/hooks';
+import React, { useCallback } from 'react';
+import { useAppDispatch } from '@/hooks/hooks';
 import {
   Content,
   setHeadersEditorContent,
 } from '@/store/reducers/headersEditorSlice';
 import Box from '@mui/material/Box';
-import { Alert, Fade } from '@mui/material';
 import SecondaryEditor from '@/components/SecondaryEditor/SecondaryEditor';
-import { checkHeadersAccepted } from '@/utils/checkHeadersAccepted';
-import { useLanguageContext } from '@/context';
-import { Keys, LANGUAGES } from '@/constants/dictionaries';
 
 export default function HeadersEditor() {
   const dispatch = useAppDispatch();
-  const { language } = useLanguageContext();
-
-  const { acceptedHeaders } = useAppSelector((state) => state.apiEndpoint);
-
-  const [headersError, setHeadersError] = useState<string | null>(null);
 
   const setValue = useCallback(
     (value: Content) => {
-      if (!value) {
-        dispatch(setHeadersEditorContent(null));
-        setHeadersError(null);
-
-        return;
-      }
-
-      const checkHeadersResult = checkHeadersAccepted(value, acceptedHeaders);
-
-      if (checkHeadersResult.isOk) {
-        dispatch(setHeadersEditorContent(value));
-        setHeadersError(null);
-
-        return;
-      }
-
-      const errorMessage =
-        checkHeadersResult.messageKey === Keys.HEADERS_SERVER_NOT_ACCEPT_ANY
-          ? LANGUAGES[language].HEADERS_SERVER_NOT_ACCEPT_ANY
-          : `${LANGUAGES[language].HEADERS_SERVER_ACCEPT_ONLY} ${(
-              acceptedHeaders as string[]
-            ).join(', ')}`;
-      setHeadersError(errorMessage);
+      dispatch(setHeadersEditorContent(value));
     },
-    [acceptedHeaders, dispatch, language],
+    [dispatch],
   );
 
   return (
     <Box>
       <SecondaryEditor setValue={setValue} />
-      <Fade in={!!headersError}>
-        <Alert severity="error">{headersError}</Alert>
-      </Fade>
     </Box>
   );
 }
