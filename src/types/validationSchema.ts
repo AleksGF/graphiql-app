8c4538966ea0e3b05d1245514273bdcda6fbb5de
DanilWork <bogdanovdanila1404@gmail.com>
2023-12-20 16:43:26 +0500

    fix: add support unicode validation

--- src/types/validationSchema.ts
+++ src/types/validationSchema.ts
@@ -8,13 +8,13 @@ export const schema = yup.object().shape({
     .string()
     .required(Keys.USER_FORM_EMAIL_REQUIRED)
     .email(Keys.USER_FORM_EMAIL_INVALID)
-    .matches(/@[^.]*\./, Keys.USER_FORM_EMAIL_INVALID),
+    .matches(/@[^.]*\.\S{2,}/, Keys.USER_FORM_EMAIL_INVALID),
   password: yup
     .string()
     .required(Keys.USER_FORM_PASSWORD_REQUIRED)
     .matches(/[0-9]/, Keys.USER_FORM_PASSWORD_NUMBER)
-    .matches(/[a-zа-я]/, Keys.USER_FORM_PASSWORD_DOWN_LETTER)
-    .matches(/[A-ZА-Я]/, Keys.USER_FORM_PASSWORD_UP_LETTER)
+    .matches(/\p{Ll}/u, Keys.USER_FORM_PASSWORD_DOWN_LETTER)
+    .matches(/\p{Lu}/u, Keys.USER_FORM_PASSWORD_UP_LETTER)
     .matches(specialCharacters, Keys.USER_FORM_PASSWORD_SPECIAL)
     .min(8, Keys.USER_FORM_PASSWORD_LENGTH),
 });
