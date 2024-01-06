89e374cc272d3db6b1c3ca839a8d86fc8eb083ff
AleksGF <aleksgf@mail.zp.ua>
2024-01-06 14:34:14 +0200

    fix: fix codemirror width

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,3 +1,4 @@
+import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import {
   EndpointEditor,
@@ -8,7 +9,6 @@ import {
   DocDrawer,
 } from '@/components';
 import { Colors } from '@/constants/colors';
-import { Container } from '@mui/material';
 
 const BLOCK_MIN_WIDTH = '260px';
 
@@ -55,13 +55,18 @@ export default function MainPage() {
               background: palette.background.default,
             })}
           >
-            <Box sx={{ flexGrow: '1' }}>
+            <Box sx={{ flexGrow: '1', position: 'relative' }}>
               <QueryEditor />
             </Box>
             <EditorAccordion />
           </Box>
           <Box
-            sx={{ width: 2 / 5, minWidth: BLOCK_MIN_WIDTH, flexGrow: 1, p: 1 }}
+            sx={{
+              width: 2 / 5,
+              minWidth: BLOCK_MIN_WIDTH,
+              flexGrow: 1,
+              p: 1,
+            }}
           >
             <ResponseViewer />
           </Box>
